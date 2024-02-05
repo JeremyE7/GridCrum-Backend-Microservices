08ff957f04a332273730181841e2c6be679136b5
Jeremy <encaladajeremy@gmail.com>
2024-02-05 09:02:14 -0500

    Refactor getAllProjects and add createProject method

--- apps/project/src/project.service.ts
+++ apps/project/src/project.service.ts
@@ -1,14 +1,12 @@
 import { PrismaService } from '@app/prisma'
 import { Injectable } from '@nestjs/common'
-import { Item } from '@prisma/client'
+import { Item, Project } from '@prisma/client'
 
 @Injectable()
 export class ProjectService {
   constructor(private prisma: PrismaService) {}
 
-  async getAllProejcts(): Promise<
-    { msg: string; projects: Item[] } | { msg: string; error: any }
-  > {
+  async getAllProejcts(): Promise<{ msg: string; projects: Item[] } | { msg: string; error: any }> {
     try {
       const projects = await this.prisma.item.findMany({
         where: {
@@ -21,5 +19,13 @@ export class ProjectService {
     } catch (error) {
       return { msg: 'Error en la solicitud', error: error.errors }
     }
+  }
+
+  async createProject(projectAux: Project): Promise<{ msg: string; project: Project; error?: string }> {
+    const project = await this.prisma.project.create({
+      data: projectAux,
+    })
+
+    return { msg: 'Proyecto creado', project }
   }
 }
